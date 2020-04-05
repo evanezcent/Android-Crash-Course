c55b02266dca412a3419343af0c1ce584defd005
Pratama Yoga <pratamayo16@gmail.com>
2020-04-05 10:59:35 +0700

    Learn Intent

---
+++ Dicoding-second/app/src/main/java/com/dicoding/picodiploma/dicoding_second/MovedActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.dicoding_second
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_moved)
+    }
+}
